6fcea5e6a83047cecb9514fc8ec05af769aa1328
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2018-12-03 15:34:03 +0500

    Update search.inc.php

--- modules/camshoter/search.inc.php
+++ modules/camshoter/search.inc.php
@@ -41,6 +41,8 @@
   // SEARCH RESULTS
   $res=SQLSelect("SELECT camshoter_devices.*, DATE_FORMAT(FROM_UNIXTIME(TS), '%d-%m-%Y %H:%m') as TS3 FROM camshoter_devices WHERE $qry ORDER BY ".$sortby_snmpdevices);
   $days=$res[0]['SROK'];
+  $iddev=$res[0]['ID'];
+
   if ($res[0]['ID']) {
    colorizeArray($res);
    $total=count($res);
@@ -50,6 +52,11 @@
    }
    $out['RESULT']=$res;
 
+//////////////////////////
+//////////////////////////
+
+  if ($this->tab=='devcount') {
+
 
 $date1=date('Y-m-d');
 $date2=strtotime("-$days day");
@@ -57,8 +64,28 @@ $date2=strtotime("-$days day");
 
 
 $arcar=$this->createDateRangeArray(date('Y-m-d',$date2), $date1);
-print_r($arcar);
+//print_r($arcar);
 $out['ARCDATES']=$arcar;
+///////////////////
+
+
+$arcar=$this->createDateRangeArray(date('Y-m-d',$date2), $date1);
+//print_r($arcar);
+$out['FILES']=$arcar;
+
+
+
+$savepath=ROOT."cms/cached/nvr/cam".$iddev;
+$folderdata=date('Ymd');
+//$folderdata='20181203';
+
+//echo $savepath."<br>";
+
+$files=$this->getfiles($savepath,$folderdata);
+//print_r($files);
+$out['FILES']=$files;
+
+}
 
 
 
