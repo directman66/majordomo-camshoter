938839b75928bc1caf0a5afd400422be4c2051ed
DMITRIY SANNIKOV <sannikovdi@yandex.ru>
2018-12-12 16:29:15 +0500

    Update adduser.inc.php

--- modules/camshoter/adduser.inc.php
+++ modules/camshoter/adduser.inc.php
@@ -21,15 +21,15 @@ mkdir($users, 0777, true);}
 $uploaddir = $users;
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
-echo '<pre>';
-if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    echo "Файл корректен и был успешно загружен.\n";
-} else {
+//echo '<pre>';
+if (!move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+//    echo "Файл корректен и был успешно загружен.\n";
+//} else {
     echo "Возможная атака с помощью файловой загрузки!\n";
 }
 
-echo 'Некоторая отладочная информация:';
-print_r($_FILES);
+//echo 'Некоторая отладочная информация:';
+//print_r($_FILES);
 
-print "</pre>";
+//print "</pre>";
 
